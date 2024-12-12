678b6dba915e60c8e229dabc39a254c216091910
Willames Santos <willamessantos@live.com>
2024-12-12 10:01:37 -0300

    aula 02 fim

--- next-front/src/app/new-route/MapNewRoute.tsx
+++ next-front/src/app/new-route/MapNewRoute.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import { useMap } from "../../hooks/useMap";
-import { DirectionsData } from "../../utils/models";
+import type { DirectionsData } from "../../utils/models";
 
 export type MapNewRouteProps = {
   directionsData: DirectionsData;
